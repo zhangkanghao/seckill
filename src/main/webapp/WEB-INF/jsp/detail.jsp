65c2d4a277e3b7fa9e3cc0193a4b92f6f183846a
zhangkanghao <zhangkanghao@imudges.com>
2018-03-09 17:00:56 +0800

    seckill system

--- src/main/webapp/WEB-INF/jsp/detail.jsp
+++ src/main/webapp/WEB-INF/jsp/detail.jsp
@@ -12,7 +12,7 @@
             <div class="panel-heading"><h1>${seckill.name}</h1></div>
         </div>
         <div class="panel-body">
-            <h2 class="text-danger">
+            <h2 class="text-danger text-center">
                 <!--显示time图标-->
                 <span class="glyphicon glyphicon-time"></span>
                 <!--展示倒计时-->
@@ -38,7 +38,7 @@
                 </div>
 
                 <div class="modal-footer">
-                    <！--验证信息-->
+                    <!--验证信息-->
                     <span id="killPhoneMessage" class="glyphicon"></span>
                     <button type="button" id="killPhoneBtn" class="btn btn-success">
                         <span class="glyphicon glyphicon-phone"></span>
@@ -60,7 +60,7 @@
 <!--jQuery cookie操作插件-->
 <script src="https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
 <!--jQUery countDown 倒计时插件-->
-<script src="https://cdn.bootcss.com/jquery-countdown/2.1.0/jquery.countdown.min.js"></script>
+<script src="https://cdn.bootcss.com/jquery.countdown/2.1.0/jquery.countdown.min.js"></script>
 <!--开始编写交互逻辑-->
 <script src="/resources/script/seckill.js" type="text/javascript"></script>
 <script type="text/javascript">
--- src/main/webapp/resources/script/seckill.js
+++ src/main/webapp/resources/script/seckill.js
@@ -1,50 +1,88 @@
 //存放主要交互逻辑js代码
 //javascript  要模块化
-var seckill={
+var seckill;
+seckill = {
     //封装秒杀相关ajax的url
-    URL:{
-
+    URL: {
+        now: function () {
+            return '/seckill/time/now';
+        }
     },
     //验证手机号,isNaN   isNot a Number 判断是否非数字
-    validatePhone: function (phone){
-        if(phone && phone.length==11 &&!isNaN(phone)){
+    validatePhone: function (phone) {
+        if (phone && phone.length == 11 && !isNaN(phone)) {
             return true;
-        }else return false;
+        } else {
+            return false;
+        }
+    },
+    countdown: function (seckillId, nowTime, startTime, endTime) {
+        var seckillBox =$('#seckill-box');
+        //时间判断
+        if (nowTime > endTime) {
+            //秒杀结束
+            seckillBox.html('秒杀结束！')
+        } else if(nowTime<startTime){
+            //未开始
+            var killTime=new Date(startTime+1000);
+            seckillBox.countdown(killTime,function (event) {
+                var format=event.strftime('秒杀倒计时：%D天 %H时 %M分 %S秒');
+                seckillBox.html(format);
+                //时间完成后毁掉时间
+            }).on('finish.countdown',function () {
+                //获取秒杀地址
+            });
+        }else{
+
+        }
     },
     //详情页秒杀逻辑
-    detail:{
+    detail: {
         //详情页初始化
-        init:function (params) {
+        init: function (params) {
             //手机验证和登录，计时交互
             //规划交互流程
             //在cookie中查找手机号
             var killPhone = $.cookie('killPhone');
-            var startTime=params['startTime'];
-            var endTime=params['endTime'];
-            var seckillId=params['seckillId'];
+
             //验证手机号
-            if (seckill.validatePhone(killPhone)) {
+            if (!seckill.validatePhone(killPhone)) {
                 //绑定手机号，控制输出
-                var killPhoneModal =$('#killPhoneModal');
+                var killPhoneModal = $('#killPhoneModal');
                 //显示弹出层
                 killPhoneModal.modal({
-                    show:true,//显示弹出层
-                    backdrop:'static',//禁止位置关闭，就是点击弹出层以外的界面关闭
-                    keyboard:false//禁止用键盘关闭弹出层
+                    show: true,//显示弹出层
+                    backdrop: 'static',//禁止位置关闭，就是点击弹出层以外的界面关闭
+                    keyboard: false//禁止用键盘关闭弹出层
                 });
-                $('#killPhoneBtn').click(function(){
-                    var inputPhone=$('#killPhoneKey').val();
-                    if(seckill.validatePhone(inputPhone)){
+                $('#killPhoneBtn').click(function () {
+                    var inputPhone = $('#killPhoneKey').val();
+                    if (seckill.validatePhone(inputPhone)) {
                         //电话写入cookie，刷新页面
-                        $.cookie('killPhone',inputPhone,{expires:7,path:'/seckill'});
+                        $.cookie('killPhone', inputPhone, {expires: 7, path: '/seckill'});
                         window.location.reload();
-                    }else{
+                    } else {
                         $('#killPhoneMessage').hide().html('<label class="label label-danger">手机号错误!</label>').show(300);
                     }
                 });
             }
 
+
+            //已经登录，显示倒计时
+            var startTime = params['startTime'];
+            var endTime = params['endTime'];
+            var seckillId = params['seckillId'];
+            $.get(seckill.URL.now(), {}, function (result) {
+                if (result && result['success']) {
+                    var nowTime = result['data'];
+                    //时间判断
+                    seckill.countdown(seckillId, nowTime, startTime, endTime);
+                } else {
+                    console.log('result' + result);
+                }
+            });
+
         }
 
     }
-}
+};
